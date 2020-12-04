693b4d9b525e9e4a6ed745dfb1bf56f5a6bff63c
Zealll <eriznis@gmail.com>
2020-12-04 02:31:03 -0500

    :zap: V2, helper function with speed improvements

--- Day 4/main.py
+++ Day 4/main.py
@@ -5,6 +5,10 @@ arr = []
 for each in paths:
     arr.append(each.split())
 
+# Correct Answers:
+    # - Part 1: 216
+    # - Part 2: 150
+
 
 
 filteredPass = [[]]
@@ -25,9 +29,85 @@ check = {
     "pid": True
 }
 
+# ======= VERSION 2 =======
+
+
+def main():
+    count = 0
+    for i in filteredPass:
+        tempArr = []
+        validFormat = True
+        allFields = True
+        for j in i:
+            x, y = j.split(':')
+            validFormat, field = validator(x, y)
+            if validFormat:
+                tempArr.append(field)
+            else:
+                continue
+
+        if not validFormat:
+            continue
+        
+        for k in check:
+            if k not in tempArr:
+                allFields = False
+        
+        if allFields:
+            count += 1
+
+    print(count)
 
 
 
+
+
+def validator(x, y):
+    key = None
+    validFormat = True
+    if x == 'byr':
+        if int(y) < 1920 or int(y) > 2002:
+            return [False, '']
+    if x == 'iyr':
+        if int(y) < 2010 or int(y) > 2020:
+            return [False, '']
+    if x == 'eyr':
+        if int(y) < 2020 or int(y) > 2030:
+            return [False, '']
+    if x == 'hgt':
+        if y[len(y) - 2:] == 'in':
+            if int(y[:len(y) - 2]) < 59 or int(y[:len(y) - 2]) > 76:
+                return [False, '']
+        elif y[len(y) - 2:] == 'cm':
+            if int(y[:len(y) - 2]) < 150 or int(y[:len(y) - 2]) > 193:
+                return [False, '']
+        else:
+            return [False, '']
+    if x == 'hcl':
+        letterArr = ['#', 'a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        if y[0] != '#': 
+            return [False, '']
+        for letter in y:
+            if letter not in letterArr:
+                
+                return [False, '']
+    if x == 'ecl':
+        colorArr = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+        if y not in colorArr:
+            return [False, '']
+    if x == 'pid':
+        if len(y) != 9:
+            return [False, '']
+        for num in y:
+            if not num.isdigit():
+                return [False, '']
+    key = x
+    return [validFormat, key]
+    
+
+    
+if __name__ == '__main__':
+    main()
 
 
 

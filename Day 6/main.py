e12ee33e65e100192d173ef71e0e588b18269148
Zealll <eriznis@gmail.com>
2020-12-06 02:03:52 -0500

    :zap: Optimized memory usage

--- Day 6/main.py
+++ Day 6/main.py
@@ -1,7 +1,7 @@
-testGroup = open('testGroup.txt', 'r')
+testGroups = open('testGroups.txt', 'r')
 
 arr = []
-for group in testGroup:
+for group in testGroups:
     arr.append(group.split())
 
 combinedGroups = [[]]
@@ -18,22 +18,28 @@ for i in arr:
 
 # ======= Part 2 =======
 total = 0
-for i in combinedGroups:
-    if len(i) == 1:
-        letters = ''
-        for letter in range(len(i[0])):
-            if i[0][letter] not in letters:
-                letters += i[0][letter]
-        total += len(letters)
-    else:
-        exampleString = [i[0]]
-        for group in range(1, len(i)):
-            exampleString.append([])
-            for letter in i[group]:
-                if letter in exampleString[len(exampleString) - 2] and letter not in exampleString[len(exampleString) - 1]:
-                        exampleString[len(exampleString) - 1].append(letter)
+for groups in combinedGroups:
+    if len(groups) == 1:
+        asnwers = ''
 
-        total += len(exampleString[len(exampleString) - 1])
+        for ansIndx in range(len(groups[0])):
+            if groups[0][ansIndx] not in asnwers:
+                asnwers += groups[0][ansIndx]
+
+        total += len(asnwers)
+    else:
+        exampleString = groups[0]
+
+        for groupIndx in range(1, len(groups)):
+            tempString = ''
+
+            for answer in groups[groupIndx]:
+                if answer in exampleString and answer not in tempString:
+                        tempString += answer
+                        
+            exampleString = tempString
+
+        total += len(exampleString)
 
 print(total)
 

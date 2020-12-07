5f38e19919b26741703170c8874890f7bd7027d0
Zealll <eriznis@gmail.com>
2020-12-07 01:44:19 -0500

    :sparkles: Added Part 2

--- Day 7/main.py
+++ Day 7/main.py
@@ -6,35 +6,46 @@ for group in test:
     
 # Correct Answers:
     # - Part 1: 268
-    # - Part 2: 
+    # - Part 2: 7867
 
-colorsWithGold = ['shiny gold']
 
-while True:
-    length = len(colorsWithGold)
-    
+# ======= Part 2 =======
+tempBags = [[1, 'shiny gold']]
+
+counter = 0
+sec = 0
+while tempBags:
+    currBag = tempBags[0]
+
     for i in arr:
         firstPart = ' '.join(i[:2])
-        secondPart = ' '.join(i[2:])
+        secondPart = i[2:]
 
-        for j in colorsWithGold:
-            if j in secondPart and firstPart not in colorsWithGold:
-                colorsWithGold.append(firstPart)
-    if length == len(colorsWithGold):
-        break
+        if currBag[1] == firstPart:
+            for j in range(len(secondPart)):
+                if secondPart[j].isdigit():
+                    counter += int(secondPart[j]) * currBag[0]
+                    tempBags.append([int(secondPart[j]) * currBag[0], secondPart[j + 1] + ' ' + secondPart[j + 2]])
+    tempBags.pop(0)
 
-print(len(colorsWithGold) - 1)
-# combinedGroups = [[]]
-# for i in arr:
-#     if len(i) == 0:
-#         combinedGroups.append(i)
-#     else:
-#         combinedGroups[len(combinedGroups) - 1] += i
+print(counter)
 
 
 
+# ======= Part 1 =======
+# colorsWithGold = ['shiny gold']
 
+# while True:
+#     length = len(colorsWithGold)
+    
+#     for i in arr:
+#         firstPart = ' '.join(i[:2])
+#         secondPart = ' '.join(i[2:])
 
+#         for j in colorsWithGold:
+#             if j in secondPart and firstPart not in colorsWithGold:
+#                 colorsWithGold.append(firstPart)
+#     if length == len(colorsWithGold):
+#         break
 
-
-
+# print(len(colorsWithGold) - 1)

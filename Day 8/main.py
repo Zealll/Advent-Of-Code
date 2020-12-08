9645e6e74a0b6c8796f4c4c42f326eae0f5c4e83
Zealll <eriznis@gmail.com>
2020-12-08 02:25:16 -0500

    :sparkles: Added Part 2 of Day 8

--- Day 8/main.py
+++ Day 8/main.py
@@ -9,22 +9,62 @@ for group in test:
     # - Part 2: 7867
 
 
+# ======= Part 2 =======
+def checker(array):
+    visited = []
+    acc = 0
+    index = 0
+    while index not in visited and index < len(array):
+        visited.append(index)
+        x, y = array[index]
+        index += 1
+
+        if x == 'nop':
+            continue
+        
+        if x == 'jmp':
+            index += int(y) - 1
+            continue
+
+        acc += int(y)
+    return index == len(array), acc
+
+
+for i in range(len(arr)):
+    x, y = arr[i]
+    
+    if x == 'acc':
+        continue
+
+    swapBetween = ['jmp', 'nop']
+    k, l = swapBetween.index(x), abs(swapBetween.index(x) - 1)
+
+    arr[i][0] = swapBetween[l]
+
+    finished, total = checker(arr)
+
+    arr[i][0] = swapBetween[k]
+
+    if finished:
+        print(total)
+        break
+
+
+
 
 # ======= Part 1 =======
-visited = []
-acc = 0
-index = 0
-while index not in visited:
-    visited.append(index)
-    x, y = arr[index]
-    index += 1
-    if x == 'nop':
-        continue
+# while index not in visited:
+#     visited.append(index)
+#     x, y = arr[index]
+#     index += 1
+#     if x == 'nop':
+#         continue
     
-    if x == 'jmp':
-        index += int(y) - 1
-        continue
+#     if x == 'jmp':
+#         index += int(y) - 1
+#         continue
 
-    acc += int(y)
+#     acc += int(y)
 
-print(acc)
+# print(index)
+# print(acc)
